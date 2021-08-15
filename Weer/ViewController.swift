98f58073e746508df3adac36dd23ca79ec6d44cd
Artyom <artyom.jalilov@gmail.com>
2021-08-16 00:10:44 +0300

    Initial Commit

---
+++ Weer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weer
+//
+//  Created by Artyom Jalilov on 16.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
